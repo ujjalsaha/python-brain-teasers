96acd2d680c2cf4410609676fb8be5a30f42926e
Ujjal Saha <ujjal.ujjal@gmail.com>
2019-09-16 15:32:54 -0600

    Added doctest

--- print_pattern1.py
+++ print_pattern1.py
@@ -1,24 +1,20 @@
-# PROBLEM: Based on input param "count", print the pattern
-"""
-count = 3
-*
-**
-***
-**
-*
-"""
-"""
-count = 5
-*
-**
-***
-****
-*****
-****
-***
-**
-*
-"""
-# SOLUTION: Using List comprehension, find the solution of increment and decrement and join them together.
-print("\n".join(['*'*i for i in range(1,count+1)]) + "\n" + "\n".join(['*'*i for i in range(count-1,0,-1)]))
+def reflective_pattern(count=7):
+    """Based on input param "count", prints the pattern
+    >>> reflective_pattern(5)
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+    """
 
+    # SOLUTION: Using List comprehension, find upper increment and lower decrement decrement sections and join them together
+    print("\n".join(['*'*i for i in range(1,count+1)]) + "\n" + "\n".join(['*'*i for i in range(count-1,0,-1)]))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
